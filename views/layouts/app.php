c1c970fbe362a1295e1185f1e002af037abd393a
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-15 10:10:02 -0300

    style: load booking and management experience styles

--- views/layouts/app.php
+++ views/layouts/app.php
@@ -18,6 +18,7 @@ $firstName = $user ? (explode(' ', trim((string) $user['name']))[0] ?? $user['na
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css" rel="stylesheet">
   <link href="<?= e(url('/assets/css/app.css')) ?>" rel="stylesheet">
+  <link href="<?= e(url('/assets/css/experience.css')) ?>" rel="stylesheet">
 </head>
 <body>
 <nav class="navbar navbar-expand-lg sticky-top">
