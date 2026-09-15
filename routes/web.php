be4fd1e109f3e55ba5ac7b7266ed00647dc9a91f
Miguel Almeida <100483395+MiguelAlmeidaJ@users.noreply.github.com>
2026-09-15 13:41:49 -0300

    feat: adiciona rotas de clientes e agendamento manual

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use App\Http\Controllers\AgendaController;
 use App\Http\Controllers\AppointmentController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\BookingController;
+use App\Http\Controllers\CustomerController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PanelController;
 use App\Http\Controllers\ScheduleController;
@@ -28,6 +29,11 @@ $router->post('/logout', [AuthController::class, 'logout']);
 $router->get('/painel', [PanelController::class, 'index']);
 $router->get('/painel/agenda', [AgendaController::class, 'index']);
 
+$router->get('/painel/clientes', [CustomerController::class, 'index']);
+$router->post('/painel/clientes', [CustomerController::class, 'store']);
+$router->get('/painel/clientes/{id}', [CustomerController::class, 'show']);
+$router->post('/painel/clientes/{id}', [CustomerController::class, 'update']);
+
 $router->get('/painel/servicos', [PanelController::class, 'services']);
 $router->post('/painel/servicos', [PanelController::class, 'storeService']);
 $router->post('/painel/servicos/{id}', [PanelController::class, 'updateService']);
@@ -46,6 +52,9 @@ $router->post('/painel/horarios/especiais', [ScheduleController::class, 'storeSp
 $router->post('/painel/horarios/especiais/{id}/remover', [ScheduleController::class, 'deleteSpecialHours']);
 
 $router->get('/painel/agendamentos', [PanelController::class, 'appointments']);
+$router->get('/painel/agendamentos/novo', [AppointmentController::class, 'create']);
+$router->get('/painel/agendamentos/novo/horarios', [AppointmentController::class, 'newAvailability']);
+$router->post('/painel/agendamentos/novo', [AppointmentController::class, 'store']);
 $router->post('/painel/agendamentos/{id}/status', [AppointmentController::class, 'status']);
 $router->get('/painel/agendamentos/{id}/editar', [AppointmentController::class, 'edit']);
 $router->get('/painel/agendamentos/{id}/horarios', [AppointmentController::class, 'availability']);
